d522a8909feb060ca2a62d96d5c61b5bc4cf4733
Travis Baker <baker.travis.w@gmail.com>
2024-12-05 04:23:57 +0000

    switch to console log

--- src/Okimat/okimat.ts
+++ src/Okimat/okimat.ts
@@ -36,7 +36,7 @@ export const okimat = async (mqtt: IMQTTConnection, esphome: IESPConnection) => 
 
     const services = await getServices();
 
-    logInfo('[okimat] Services', services);
+    console.log('[okimat] Services', services);
     const service = services.find((s) => s.uuid === '62741523-52f9-8864-b1ab-3b3a8d65950b');
     if (!service) {
       logInfo('[Okimat] Could not find expected services for device:', name);
